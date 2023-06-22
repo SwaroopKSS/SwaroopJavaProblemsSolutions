bc83e119ce55137019eeb6f4b08b2180756132db
SwaroopKSS <swaroopdaretodo@gmail.com>
2023-06-22 11:18:43 +0530

    commiting the latest problems

--- src/com/swaroop/dsa/harshasir/matrix2d1/MaximumSubArraySumOfFixedLength.java
+++ src/com/swaroop/dsa/harshasir/matrix2d1/MaximumSubArraySumOfFixedLength.java
@@ -1,4 +1,90 @@
 package com.swaroop.dsa.harshasir.matrix2d1;
 
+
+/*
+
+Q2. Maximum subarray sum of fixed length
+
+Problem Description
+        Given an array A of length N, Find the maximum subarray sum out of all possible subarray of length B.
+
+
+        Problem Constraints
+        1 <= N <= 105
+        1 <= A[i] <= 106
+
+
+        Input Format
+        First argument A is an array of integers.
+        Second argument B is length of subarray.
+
+
+        Output Format
+        Return a single integer i.e. max subarray sum
+
+
+        Example Input
+        Input 1:
+
+        A = [6, 7, 8, 2]
+        B = 2
+        Input 2:
+
+        A = [3, 9, 5, 6, 5, 11]
+        B = 3
+
+
+        Example Output
+        Output 1:
+
+        15
+        Output 2:
+
+        22
+
+
+        Example Explanation
+        Explanation 1:
+
+        Subarray with maximum sum is [7, 8] with sum 15.
+        Explanation 2:
+
+        Subarray with maximum sum is [6, 5, 11] with sum 22.
+
+*/
 public class MaximumSubArraySumOfFixedLength {
+
+    public long solve(int[] A, int B) {
+
+
+        //B is lenth of subarray
+
+        int s = 0;
+        int e = B-1;
+
+        long sum = 0;
+
+        long maxSum = Long.MIN_VALUE;
+
+        for(int i=0;i<=e;i++){
+            sum = sum + A[i];
+        }
+
+        s++; e++;
+
+        maxSum = Math.max(maxSum,sum);
+
+
+        while(e<A.length){
+            sum = sum - A[s-1] + A[e];
+
+            maxSum = Math.max(maxSum,sum);
+
+            s++;e++;
+
+        }
+        return maxSum;
+
+    }
+
 }
--- src/com/swaroop/dsa/harshasir/matrix2d1/SubArrayWithGivenSumAndLength.java
+++ src/com/swaroop/dsa/harshasir/matrix2d1/SubArrayWithGivenSumAndLength.java
@@ -1,4 +1,115 @@
 package com.swaroop.dsa.harshasir.matrix2d1;
 
+/*
+
+Subarray with given sum and length
+
+Problem Description
+        Given an array A of length N. Also given are integers B and C.
+
+        Return 1 if there exists a subarray with length B having sum C and 0 otherwise
+
+
+
+        Problem Constraints
+        1 <= N <= 105
+
+        1 <= A[i] <= 104
+
+        1 <= B <= N
+
+        1 <= C <= 109
+
+
+
+        Input Format
+        First argument A is an array of integers.
+
+        The remaining arguments B and C are integers
+
+
+
+        Output Format
+        Return 1 if such a subarray exist and 0 otherwise
+
+
+        Example Input
+        Input 1:
+        A = [4, 3, 2, 6, 1]
+        B = 3
+        C = 11
+        Input 2:
+
+        A = [4, 2, 2, 5, 1]
+        B = 4
+        C = 6
+
+
+        Example Output
+        Output 1:
+        1
+        Output 2:
+
+        0
+
+*/
 public class SubArrayWithGivenSumAndLength {
+
+
+    public static void main(String args[]){
+
+        int[] A = {6,3,3,6,7,8,7,3,7};
+
+        //int[] A = {4,3,2,6,1};
+
+        int B = 2;
+
+       // int B = 3;
+
+        int C = 10;
+
+       // int C = 11;
+
+
+
+        System.out.println(solve(A,B,C));
+    }
+
+
+//sliding window technique. calculating sum of length
+
+    public static int solve(int[] A, int B, int C) {
+
+        //B is lenth of subarray
+        //C sum to match with
+
+        int s = 0;
+        int e = B-1;
+
+        long sum = 0;
+
+        for(int i=0;i<=e;i++){
+            sum = sum + A[i];
+        }
+
+        s++; e++;
+
+        if(sum == C){
+            return 1;
+        }
+
+
+        while(e<A.length){
+            sum = sum - A[s-1] + A[e];
+
+            if(sum==C){
+                return 1;
+            }
+
+            s++;e++;
+
+        }
+        return 0;
+    }
+
 }
